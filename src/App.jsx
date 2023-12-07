61284bef1016b10bfa8736f4e9bc8e03ebec7eba
monicahillman <mmhillman1@ucs.br>
2023-12-06 23:20:54 -0300

    aula 03

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import BarraDePesquisa from './componentes/BarraDePesquisa'
+import Filtro from './componentes/Filtro'
 import Sidebar from './componentes/Sidebar'
 
 function App() {
 
   return (
-    <Sidebar />
+    <div className='container'>
+      <Sidebar />
+      <div>
+        <BarraDePesquisa />
+        <Filtro />
+      </div>
+    </div>
   )
 }
 
